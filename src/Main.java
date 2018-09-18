2560c8d4faf4bcb3fcb738ab5bf7bbfe4d352434
divyah3 <divyah3@illinois.edu>
2018-09-18 12:23:35 -0500

    Printing website text.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("here");
+        String text = (urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(text);
+        //System.out.println(words(text));
     }
+
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+   /* public static int words(String website) {
+        int countLines = 0;
+        for (String[] lines: website) {
+            countLines++;
+        }
+        return countLines;
+
+    }*/
 }
